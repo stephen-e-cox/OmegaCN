d2bb9045ffb1c573579c389b0cee8eac87306cc7
stephen-e-cox <cox.stephen.e@gmail.com>
2019-10-09 15:15:09 -0400

    TIL that concatenating is bad, but also .format does not work on bytestrings. Not sure if this is the best solution but it works.

--- device_functions.py
+++ device_functions.py
@@ -24,7 +24,7 @@ class OmegaCN740:
     def _ask(self, message):
         self.instrument.write(message)
         response = self.instrument.readline()
-        message = response[response.index(self.slave + b'03')+4:-4]
+        message = response[response.index(b''.join([self.slave, b'03']))+4:-4]
         message_length = int(message[0:2])
         if message_length == 4:
             temperature = message[2:6]
@@ -44,11 +44,11 @@ class OmegaCN740:
         return bytes(checksum.zfill(2).upper(), 'utf-8')
 
     def _read_temp(self):
-        message = self.slave + b'03' + b'4700' + b'0002'
-        message = b':' + message + self._compute_lrc(message) + b'\r\n'
+        message = b''.join([self.slave, b'03', b'4700', b'0002'])
+        message = b''.join([b':', message, self._compute_lrc(message), b'\r\n'])
         return message
 
     def _write_setpoint(self, temperature):
-        message = self.slave + b'06' + b'4701' + bytes(hex(temperature)[2:].zfill(4).upper(), 'utf-8')
-        message = b':' + message + self._compute_lrc(message) + b'\r\n'
+        message = b''.join([self.slave, b'06', b'4701', bytes(hex(temperature)[2:].zfill(4).upper(), 'utf-8')])
+        message = b''.join([b':', message, self._compute_lrc(message), b'\r\n'])
         return message
