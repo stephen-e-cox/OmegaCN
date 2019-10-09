0ea29bb11823c2bae374acd88447978de0ad9cd3
stephen-e-cox <cox.stephen.e@gmail.com>
2019-10-09 13:21:57 -0400

    Cleanup and working filename creation

--- read_temp.py
+++ read_temp.py
@@ -1,9 +1,4 @@
-from device_functions import OmegaCN740
-
-instrument = OmegaCN740(slave=3)
-temperature, setpoint = instrument.read_temp()
-print(temperature)
-print(setpoint)
+from random import random
 
 import numpy as np
 from traits.api import HasTraits, Str, Int, Float, Enum, Array, Button, Instance
@@ -11,11 +6,22 @@ from traitsui.api import View, Item, HGroup, VGroup, spring, Handler
 from chaco.api import Plot
 from chaco.chaco_plot_editor import ChacoPlotItem
 from pyface.timer.api import Timer
-from threading import Thread, Event
 import os
 import datetime
 import time
-from logging import debug
+
+try:
+    from device_functions import OmegaCN740
+except ImportError:
+    class OmegaCN740:
+        def __init__(self, *args, **kw):
+            pass
+
+        def read_temp(self):
+            return 10*random(), random()
+
+
+INSTRUMENT = OmegaCN740(slave=3)
 
 
 class Viewer(HasTraits):
@@ -27,77 +33,78 @@ class Viewer(HasTraits):
 
     data = Array
 
-    plot_type = Enum("line", "scatter")
+    plot_type = Enum('line', 'scatter')
 
-    view = View(ChacoPlotItem("index", "data",
-                              type_trait="plot_type",
+    view = View(ChacoPlotItem('index', 'data',
+                              type_trait='plot_type',
                               resizable=True,
-                              x_label="Time",
-                              y_label="Signal",
-                              color="blue",
-                              bgcolor="white",
+                              x_label='Time',
+                              y_label='Signal',
+                              color='blue',
+                              bgcolor='white',
                               border_visible=True,
                               border_width=1,
-                              padding_bg_color="lightgray",
+                              padding_bg_color='lightgray',
                               width=800,
                               height=380,
                               marker_size=2,
                               show_label=False),
-                HGroup(spring, Item("plot_type", style='custom'), spring),
-                resizable = True,
-                buttons = ["OK"],
+                HGroup(spring, Item('plot_type', style='custom'), spring),
+                resizable=True,
+                buttons=['OK'],
                 width=800, height=500)
 
 
-class Logger(Thread, HasTraits):
+class Logger(HasTraits):
+    sample_name = Str('default')
+    sample_weight = Float
+    heating_temp = Float
+    heating_time = Int
 
-    def __init__(self, sample_name, sample_weight, heating_temp, heating_time):
-        super(Logger, self).__init__()
+    temperature = Float
+    setpoint = Float
+    current_time = Float
 
-        self.stopRequest = Event()
-        self.sample_name = sample_name
-        self.sample_weight = sample_weight
-        self.heating_temp = heating_temp
-        self.heating_time = heating_time
-        self.temperature = Float
-        self.setpoint = Float
-        self.timer = Float
-        self.timestamp = datetime.datetime.now()
-        self.started = False
+    start = None
 
-        filename = self.sample_name + '.csv'
-        header = 'time, temperature, setpoint'
-        filesize = 0
+    def __init__(self, *args, **kw):
+        super(Logger, self).__init__(*args, **kw)
 
-        if (os.path.exists(filename) and os.path.isfile(filename)):
-            filesize = os.stat(filename).st_size
-        self.f = open(filename, 'a')
+        self.timestamp = datetime.datetime.now().isoformat()
 
-        if filesize == 0:
-            self.f.write('%s\n' % header)
+        header = 'time,temperature,setpoint'
+        self.path = self._make_path()
+        with open(self.path, 'w') as wfile:
+            wfile.write('{}\n'.format(header))
 
-    def run(self):
-        start = time.time()
-        while not self.stopRequest.isSet():
-            time.sleep(0.1)
-            self.temperature, self.setpoint = instrument.read_temp()
-            self.timestamp = str(datetime.datetime.now())
-            self.timer = time.time() - start
-            self.started = True
-            self.f.write('%s\n' % [self.timestamp, self.timer, self.temperature, self.setpoint])
+    def set_start_time(self):
+        self.start = time.time()
 
-    def stop(self):
-        self.stopRequest.set()
-        time.sleep(1)
-        try:
-            self.f.close()
-        except IOError:
-            debug('File not open')
-            return
+    def update(self):
+        self.temperature, self.setpoint = INSTRUMENT.read_temp()
+        self.timestamp = datetime.datetime.now().isoformat()
+        self.current_time = time.time() - self.start
+        self._write()
+
+    def _make_path(self):
+        path = '{}.csv'.format(self.sample_name)
+        i = 1
+        while 1:
+            if os.path.isfile(path):
+                path = '{}-{:03n}.csv'.format(self.sample_name, i)
+                i += 1
+            else:
+                return path
+
+    def _write(self):
+        with open(self.path, 'a') as wfile:
+            args = [self.timestamp, self.current_time, self.temperature, self.setpoint]
+            args = [str(a) for a in args]
+            line = ','.join(args)
+            wfile.write('{}\n'.format(line))
 
 
 class StandaloneRecorder(HasTraits):
-
     viewer = Instance(Viewer)
 
     sample_name = Str('default')
@@ -106,27 +113,32 @@ class StandaloneRecorder(HasTraits):
     heating_time = Int
     plot = Instance(Plot)
 
-    temp_logger = Logger(sample_name=sample_name, sample_weight=sample_weight,
-                              heating_temp=heating_temp, heating_time=heating_time)
+    temp_logger = Instance(Logger)
 
     view = View(HGroup(VGroup(Item(name='sample_name'),
                               Item(name='sample_weight'),
                               Item(name='heating_temp'),
                               Item(name='heating_time'))), width=1000, height=500, resizable=True,
-                title="Temperature Plot")
+                title='Temperature Plot')
+
+    def _temp_logger_default(self):
+        return Logger(sample_name=self.sample_name,
+                      sample_weight=self.sample_weight,
+                      heating_temp=self.heating_temp,
+                      heating_time=self.heating_time)
 
     def timer_tick(self, *args):
-
         cur_data = self.viewer.data
         cur_index = self.viewer.index
-        if self.temp_logger.started:
-            new_data = np.hstack((cur_data, [self.temp_logger.temperature]))
-            new_index = np.hstack((cur_index, [self.temp_logger.timer]))
-            self.viewer.index = new_index
-            self.viewer.data = new_data
-            return
-        else:
-            return
+        if self.temp_logger.start is None:
+            self.temp_logger.set_start_time()
+
+        self.temp_logger.update()
+
+        new_data = np.hstack((cur_data, [self.temp_logger.temperature]))
+        new_index = np.hstack((cur_index, [self.temp_logger.current_time]))
+        self.viewer.index = new_index
+        self.viewer.data = new_data
 
 
 class DemoHandler(Handler):
@@ -144,8 +156,8 @@ class Demo(HasTraits):
     controller = Instance(StandaloneRecorder)
     viewer = Instance(Viewer, ())
     timer = Instance(Timer)
-    run = Button("Log Temps")
-    stop = Button("Stop Logging")
+    run = Button('Log Temps')
+    stop = Button('Stop Logging')
     view = View(Item('controller', style='custom', show_label=False),
                 Item(name='run', show_label=False),
                 Item(name='stop', show_label=False),
@@ -153,24 +165,16 @@ class Demo(HasTraits):
                 handler=DemoHandler,
                 resizable=True)
 
-    def configure_traits(self, *args, **kws):
-
-        return super(Demo, self).configure_traits(*args, **kws)
+    def _stop_fired(self):
+        self.timer.stop()
 
     def _run_fired(self):
-
-        self.controller.temp_logger.start()
         self.timer = Timer(100, self.controller.timer_tick)
-
-    def _stop_fired(self):
-
-        self.controller.temp_logger.stop()
 
     def _controller_default(self):
         return StandaloneRecorder(viewer=self.viewer)
 
 
-popup = Demo()
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    popup = Demo()
     popup.configure_traits()
